e59832761bb25c80092e4c528a4b095e8e082a06
RABITBABY <your_email@545306388@qq.com>
2016-11-15 18:30:09 +0800

    ppp

--- CompetitionSystem/src/com/cs/action/TestAction.java
+++ CompetitionSystem/src/com/cs/action/TestAction.java
@@ -4,7 +4,7 @@ public class TestAction {
 
 	private String s(){
 		System.out.println("lujianwei");
-		System.out.println("欢雯");
+		System.out.println("欢");
 		System.out.println("22");
 		String stringwwwwe="222";
 		return "sss";
