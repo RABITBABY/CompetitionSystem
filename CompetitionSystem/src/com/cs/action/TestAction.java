6f3f06a19642308fad70b0848d62ae9c5802b9c9
RABITBABY <your_email@545306388@qq.com>
2016-11-15 14:54:35 +0800

    test

--- CompetitionSystem/src/com/cs/action/TestAction.java
+++ CompetitionSystem/src/com/cs/action/TestAction.java
@@ -3,6 +3,6 @@ package com.cs.action;
 public class TestAction {
 
 	private String s(){
-		return "美欣快工作~~~";
+		return "美欣快工作~";
 	}
 }
--- CompetitionSystem/src/com/cs/dao/TestDao.java
+++ CompetitionSystem/src/com/cs/dao/TestDao.java
@@ -1,5 +1,7 @@
 package com.cs.dao;
 
 public class TestDao {
-
+  public void name() {
+	
+}
 }
