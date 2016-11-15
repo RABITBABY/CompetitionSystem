20101b6fb499f100f59ef440399992624f3676c8
RABITBABY <your_email@545306388@qq.com>
2016-11-15 18:20:02 +0800

    ii

--- CompetitionSystem/src/com/cs/action/TestAction.java
+++ CompetitionSystem/src/com/cs/action/TestAction.java
@@ -6,7 +6,7 @@ public class TestAction {
 		System.out.println("lujianwei");
 		System.out.println("欢雯");
 		System.out.println("22");
-		String stringhe="222";
+		String string7e="222";
 		return "sss";
 	}
 }
