ac8cbcc75c3255f03d6214cbcf6902cc8e7f71ce
RABITBABY <your_email@545306388@qq.com>
2016-11-15 18:11:50 +0800

    合并

--- CompetitionSystem/src/com/cs/action/TestAction.java
+++ CompetitionSystem/src/com/cs/action/TestAction.java
@@ -4,7 +4,7 @@ public class TestAction {
 
 	private String s(){
 		System.out.println("lujianwei");
-		System.out.println("22");
+		System.out.println("欢雯");
 		System.out.println("22");
 		String stringmy="222";
 		return "sss";
