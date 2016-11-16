075c05a62a48c4eea4e7c5b17ba6af5e35b3aab4
RABITBABY <your_email@545306388@qq.com>
2016-11-16 12:56:24 +0800

    000

--- CompetitionSystem/src/com/cs/action/TestAction.java
+++ CompetitionSystem/src/com/cs/action/TestAction.java
@@ -4,19 +4,6 @@ public class TestAction {
 
 	private String s(){
 		System.out.println("lujianwei");
-		System.out.println("欢");
-		System.out.println("22");
-		String string="225";
-		System.out.println(string);
-		System.out.println("ss");
-		System.out.println("ss");
-		System.out.println("lujianwei");
-		System.out.println("欢");
-		System.out.println("22");
-		String string2="225";
-		System.out.println(string);
-		System.out.println("ss");
-		System.out.println("ss");
 		return "s1";
 	}
 	private String s22(){
