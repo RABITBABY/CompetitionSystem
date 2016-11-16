5bb80ed322e522b1be8d06abb0d5c5de8dc36534
RABITBABY <your_email@545306388@qq.com>
2016-11-16 12:17:09 +0800

    cc

--- CompetitionSystem/src/com/cs/action/TestAction.java
+++ CompetitionSystem/src/com/cs/action/TestAction.java
@@ -6,7 +6,7 @@ public class TestAction {
 		System.out.println("lujianwei");
 		System.out.println("欢");
 		System.out.println("22");
-		String string="22";
+		String string="225";
 		System.out.println(string);
 		return "sss";
 	}
