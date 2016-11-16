cd2b540a43e3732ec15577fee591184680d22c4f
RABITBABY <your_email@545306388@qq.com>
2016-11-16 12:58:36 +0800

    88

--- CompetitionSystem/src/com/cs/action/TestAction.java
+++ CompetitionSystem/src/com/cs/action/TestAction.java
@@ -9,9 +9,7 @@ public class TestAction {
 	private String s22(){
 		System.out.println("lujianwei");
 		System.out.println("欢");
-		System.out.println("22");
-		String string="225";
-		System.out.println(string);
+		
 		return "s1";
 	}
 }
